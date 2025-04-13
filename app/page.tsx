cf14ee3ef9abeb5bde0c36527edb14273c3614c7
Ayush Sharma <ayush.sharma74892@gmail.com>
2025-04-13 20:59:34 +0530

    Khuch to kiya h

--- app/page.tsx
+++ app/page.tsx
@@ -6,31 +6,41 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <nav className=" w-full h-16 flex items-center justify-between px-10">
+      <nav className="w-full h-16 flex items-center justify-between px-4 sm:px-8 md:px-10">
         <div className="flex flex-row items-center gap-2">
-          <Image src={logo} alt="logo" width={50} height={50} />
-          <h1 className="text-white text-xl font-semibold">Hirehog</h1>
+          <Image src={logo} alt="logo" width={40} height={40} />
+          <h1 className="text-white text-lg sm:text-xl font-semibold">Hirehog</h1>
         </div>
-        <div className="flex flex-row items-center gap-3">
-          <FaGithub color="white" size={25} className="hover:scale-115 transition-all cursor-pointer" />
-          <button className="transition-all text-white bg-[#242429] text-sm hover:bg-zinc-700 py-2  cursor-pointer px-4 rounded">
+        <div className="flex flex-row items-center gap-2 sm:gap-3">
+          <FaGithub
+            color="white"
+            size={22}
+            className="hover:scale-110 transition-all cursor-pointer"
+          />
+          <button className="transition-all text-white bg-[#242429] text-sm hover:bg-zinc-700 py-2 px-3 sm:px-4 rounded">
             Login
           </button>
         </div>
       </nav>
-      <div className="bg-[#101011] gap-10 h-[calc(100vh-4rem)] relative flex-col flex items-center justify-center">
-        <div className="flex flex-col items-center gap-3">
 
-        <h1 className="text-white text-6xl font-bold px-20 text-center">
-          Job application tracking 
-        </h1>
-        <h1 className="text-white text-6xl font-bold px-20 text-center">
-          {" "}
-          Reimagined
-        </h1>
-        <p className="text-white text-center">Stay organized. Stay ahead</p>
+      <div className="bg-[#101011] gap-8 sm:gap-10 h-[calc(100vh-4rem)] relative flex flex-col items-center justify-center px-4 sm:px-6">
+        <div className="flex flex-col items-center ">
+          <h1 className="text-white text-3xl sm:text-5xl md:text-6xl font-bold text-center leading-tight">
+            Job application tracking
+          </h1>
+          <h1 className="text-white text-3xl sm:text-5xl md:text-6xl font-bold text-center leading-tight">
+            Reimagined
+          </h1>
+          <p className="text-[#b2b9b2] text-center text-sm sm:text-base md:text-lg">
+            Stay organized. Stay ahead.
+          </p>
         </div>
-        <Button size={"xl"} className="bg-[#242429]  transition-all font-semibold cursor-pointer">Get Started</Button>
+        <Button
+          size="lg"
+          className="bg-[#242429] transition-all font-semibold cursor-pointer text-sm sm:text-base px-6 py-2 sm:px-8 sm:py-3"
+        >
+          Get Started
+        </Button>
       </div>
     </>
   );
